f4510a547a916705c34525e4fd892c5fe0de5c41
Ajay <ponnuru.ajaykumar@gmail.com>
2026-06-01 13:26:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ const featuredProjects = [
   name: "Automatic Folk Music Composer",
   summary:
     "Engineered a generative AI application that creates traditional folk melodies using a character-level LSTM model trained on the Nottingham Music Database, featuring interactive music notation rendering and real-time audio playback.",
-  stack: "Python / PyTorch / Generative AI / LSTM / Streamlit",
+  stack: "Python / LSTM / Deep Learning ",
   highlight: "Generative AI Music",
   projectLink: "https://ajay-hb-folk-music-generator.streamlit.app/",
   link: "https://github.com/Ajay-hb/Folk-Music-Generator",
